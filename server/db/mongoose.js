a22209b6a679cbe34154c9b6dcfd4eeb7aebe77e
Saud Alsaeed <minesaud@gmail.com>
2017-12-01 16:16:28 +0300

    Setup app for heroku back to 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,6 @@ let db = {
   localhost:'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://saudi:0551701747@ds127506.mlab.com:27506/testing-get-todo-db'
 }
-mongoose.connect(process.env.MONGODB_URI || db.localhost);
+mongoose.connect(db.mlab || db.localhost);
 
 module.exports = {mongoose};
