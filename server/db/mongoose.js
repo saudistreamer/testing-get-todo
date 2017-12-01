1ebd8533cfa8c7860b92825c849efcacdcab5ce5
Saud Alsaeed <minesaud@gmail.com>
2017-12-01 16:48:02 +0300

    Setup app for heroku with local and mlab 2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 let db = {
-  localhost:'mongodb://localhosts:27017/TodoApp',
+  localhost:'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://saudi:0551701747@ds127506.mlab.com:27506/testing-get-todo-db'
 }
 //Comment out when you want to push to herku
-mongoose.connect(db.localhost, function(error){
+mongoose.connect(db.localhost, (error) =>{
     if(error){
     mongoose.connect(db.mlab);
-            console.log(`connection successful on db.mlab`);
+            //console.log(error);
+            return console.log(`connection successful on db.mlab`);
     }
             console.log(`connection successful on db.localhost`);
 });
