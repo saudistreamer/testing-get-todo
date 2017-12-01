480394683b7eabb9f99093c9f3672e9ee6b6457e
Saud Alsaeed <minesaud@gmail.com>
2017-12-01 16:23:51 +0300

    Setup app for heroku 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,6 +5,8 @@ let db = {
   localhost:'mongodb://localhost:27017/TodoApp',
   mlab: 'mongodb://saudi:0551701747@ds127506.mlab.com:27506/testing-get-todo-db'
 }
-mongoose.connect(db.mlab || db.localhost);
+//Comment out when you want to push to herku
+mongoose.connect(db.localhost || db.mlab);
+//mongoose.connect(db.localhost);
 
 module.exports = {mongoose};
