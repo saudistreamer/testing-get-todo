d8e4ea94ebb8a9a23a9fe4ce5d92a2b00241ab13
Saud Alsaeed <minesaud@gmail.com>
2017-12-02 16:25:26 +0300

    Changed config file to check if env===production

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,9 @@
 var env = process.env.NODE_ENV || 'development';
 
+if (env ==='production'){
+	 process.env.PORT = 27506;
+  process.env.MONGODB_URI = 'mongodb://saudi:0551701747@ds127506.mlab.com:27506/testing-get-todo-db';
+}else
 if (env === 'development') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
