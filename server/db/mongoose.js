ba0b31d1e17a941ec3e849704a451a4178b49bc3
Saud Alsaeed <minesaud@gmail.com>
2017-12-02 16:39:16 +0300

    Changed config file to check if env===production 4nd try: removed set port

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var env = process.env.NODE_ENV || 'development';
 var MONGOdb_URI = "";
 if (env ==='production'){
-	 process.env.PORT = 27506;
   MONGOdb_URI = 'mongodb://saudi:0551701747@ds127506.mlab.com:27506/testing-get-todo-db';
 }else if (env === 'development') {
   process.env.PORT = 3000;
